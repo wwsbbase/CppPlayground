837ea4cb51019b9961eb3802a9e6bdf6bec25d01
D.J. Zou <bopy2008@163.com>
2018-04-15 11:38:09 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::cout << "io" << std::endl;
+    printf("xxxx\n");
+    printf("xxxx\n");
+    printf("xxxx\n");
+    printf("xxxx");
+    int x = 100;
+    x++;
+    printf("%d", x);
+    getchar();
+    return 0;
+}
