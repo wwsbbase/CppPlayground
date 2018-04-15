aa78a896aed342db0c6e1dc88022730d6bf3c88f
D.J. Zou <bopy2008@163.com>
2018-04-15 23:09:19 +0800

    内存操作

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 #include "stdio.h"
+#include "memory.h"
 #include <iostream>
 #include <vector>
 
 int main()
 {
-    std::cout << "io" << std::endl;
-    printf("xxxx\n");
-    printf("xxxx\n");
-    printf("xxxx\n");
-    printf("xxxx");
-    int x = 100;
-    x++;
-    printf("%d", x);
+    std::cout << "### begin ###" << std::endl;
+
+    const int CACHE_DATA_LENGTH = 64 * 1024;
+    char cacheData[CACHE_DATA_LENGTH] = {'\0'};
+
+    struct messageA
+    {
+        int a;
+        int b;
+        char array[0];
+        messageA()
+        {
+            memset(this, 0, sizeof(*this));
+        }
+    };
+
+    messageA *pMsg = (messageA *)cacheData;
+
+    char message[] = "hello, little serven";
+
+    memcpy_s(pMsg->array, sizeof(message), message, sizeof(message));
+
+    std::cout << pMsg->array << std::endl;
+
+    std::cout << "### end ###" << std::endl;
     getchar();
     return 0;
 }
